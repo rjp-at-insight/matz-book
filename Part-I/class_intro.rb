84232e73fdade8d7007ff36890b788dbe3db1620
rjp-at-insight <robert.patterson@insight.com>
2020-10-04 15:18:27 -0400

    updated class_intro.rb

--- Part-I/class_intro.rb
+++ Part-I/class_intro.rb
@@ -1,16 +1,21 @@
 #   AUTHOR      : Robert James Patterson
-#   DATE        : 09/30/2020
+#   DATE        : 10/04/2020
 #   FILE        : class_intro.rb
 #   SYNOPSIS    : Work-thru file for 'Programming Ruby - The Pragmatic Programmer's Gudie'. This file is a simple
 #               intro to OOP classes.
 
 class Song
+    
+    # Class Constructor
     def initialize(title, artist, duration)
         @title      = title
         @artist     = artist
         @duration   = duration
     end
 
+    # Getter and Setter
+    attr-reader :title :artist :duration
+    attr-writer :title :artist :duration 
     def to_s
         "Song: #{@title}--#{@artist} (#{@duration})"
     end    
